9145ef417a939307dd15f46d29ba70274b94debb
19801191517 <tangwangyao0228@163.com>
2025-09-17 10:20:11 +0800

    feat: other demo

---
+++ src/other.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from './assets/jquery';
+import Highchart from './assets/highchart';
+
+console.log('ReactDOM: ', ReactDOM);
+console.log('React: ', React);
+console.log('$: ', $);
+console.log('Highchart: ', Highchart);
---
+++ src/other2.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from './assets/jquery';
+
+console.log('ReactDOM: ', ReactDOM);
+console.log('React: ', React);
+console.log('$: ', $);

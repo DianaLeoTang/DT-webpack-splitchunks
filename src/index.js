daee230c04b4ebb107bc7f6165355f87eb04804b
19801191517 <tangwangyao0228@163.com>
2025-09-17 10:18:47 +0800

    feat: about React

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// import $ from 'jquery';
+// //utils
+// import utils from './utils';
+// console.log('utils: ', utils);
+
+// import data from './data';
+// console.log('data: ', data);
+
+// import data2 from './data2';
+// console.log('data: ', data2);
+
+// import data3 from './data3';
+// console.log('data: ', data3);
+
+// console.log('$: ', $);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from './assets/jquery';
+import Highchart from './assets/highchart';
+
+console.log('ReactDOM: ', ReactDOM);
+console.log('React: ', React);
+console.log('$: ', $);
+console.log('Highlight: ', Highchart);
